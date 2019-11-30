0b1a8b7cefd55d256689cc829a470a810a8642bd
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-11-30 16:50:08 -0500

    Initial Commit

---
+++ BannersSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BannersSwiftUI
+//
+//  Created by Jean-Marc Boullianne on 11/30/19.
+//  Copyright © 2019 Jean-Marc Boullianne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
