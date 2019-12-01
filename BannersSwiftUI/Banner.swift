587d9c94c0c2cca825a176a707bf0533d795d55e
Jean-Marc Boullianne <jboullianne@gmail.com>
2019-12-01 13:32:36 -0500

    Initial Commit for V1 to Github.

---
+++ BannersSwiftUI/Banner.swift
@@ -0,0 +1,21 @@
+//
+//  Banner.swift
+//  BannersSwiftUI
+//
+//  Created by Jean-Marc Boullianne on 11/30/19.
+//  Copyright © 2019 Jean-Marc Boullianne. All rights reserved.
+//
+
+import SwiftUI
+
+struct Banner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Banner_Previews: PreviewProvider {
+    static var previews: some View {
+        Banner()
+    }
+}
